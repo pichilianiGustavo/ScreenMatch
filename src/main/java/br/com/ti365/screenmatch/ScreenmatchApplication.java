ccea582cf15417f1dc04935a1b11c6ffc312cdf8
Gustavo Giordano <pichilianig@gmail.com>
2024-03-08 09:23:28 -0300

    Main Class - Show View

--- src/main/java/br/com/ti365/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/ti365/screenmatch/ScreenmatchApplication.java
@@ -1,18 +1,10 @@
 package br.com.ti365.screenmatch;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.ti365.screenmatch.model.EpisodesData;
-import br.com.ti365.screenmatch.model.SeasonsData;
-import br.com.ti365.screenmatch.model.SeriesData;
-import br.com.ti365.screenmatch.service.ApiConsumer;
-import br.com.ti365.screenmatch.service.DataConverterImplementation;
-import br.com.ti365.screenmatch.service.interfaces.DataConverterInterface;
+import br.com.ti365.screenmatch.main.Main;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -23,24 +15,18 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		ApiConsumer apiConsumer = new ApiConsumer();
-		var json = apiConsumer.getApiData("https://www.omdbapi.com/?t=gilmore+girls&apikey=cf34e04");
-		System.out.println(json);
-		DataConverterInterface converter = new DataConverterImplementation();
-		SeriesData seriesData = converter.getData(json, SeriesData.class);
-		System.out.println(seriesData);
-		json = apiConsumer.getApiData("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=cf34e04");
-		EpisodesData episodeData = converter.getData(json, EpisodesData.class);
-		System.out.println(episodeData);
 		
-		List<SeasonsData> seasons = new ArrayList<>();
+//		List<SeasonsData> seasons = new ArrayList<>();
+//		
+//		for (int i = 1; i <= seriesData.seasons(); i++) {
+//			json = apiConsumer.getApiData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +"&apikey=cf34e04");
+//			SeasonsData seasonsData = converter.getData(json, SeasonsData.class);
+//			seasons.add(seasonsData);
+//		}
+//		seasons.forEach(System.out::println);
 		
-		for (int i = 1; i <= seriesData.seasons(); i++) {
-			json = apiConsumer.getApiData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +"&apikey=cf34e04");
-			SeasonsData seasonsData = converter.getData(json, SeasonsData.class);
-			seasons.add(seasonsData);
-		}
-		seasons.forEach(System.out::println);
+		Main mainClass = new Main();
+		mainClass.showMenu();
 	}
 
 }
---
+++ src/main/java/br/com/ti365/screenmatch/main/Main.java
@@ -0,0 +1,37 @@
+package br.com.ti365.screenmatch.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.com.ti365.screenmatch.model.SeasonsData;
+import br.com.ti365.screenmatch.model.SeriesData;
+import br.com.ti365.screenmatch.service.ApiConsumer;
+import br.com.ti365.screenmatch.service.DataConverterImplementation;
+import br.com.ti365.screenmatch.service.interfaces.DataConverterInterface;
+
+public class Main {
+	
+	private Scanner reader = new Scanner(System.in);
+	private final String BASEURL = "https://www.omdbapi.com/?t=";
+	private final String APIKEY = "&apikey=cf34e04";
+	private ApiConsumer apiConsumer = new ApiConsumer();
+	private DataConverterInterface converter = new DataConverterImplementation();
+	
+	public void showMenu() {
+		System.out.println("Digite o nome da serie que deseja buscar:");
+		var serieName = reader.nextLine();
+		var json = apiConsumer.getApiData(BASEURL + serieName.replace(" ", "+") + APIKEY);
+		SeriesData seriesData = converter.getData(json, SeriesData.class);
+		System.out.println(seriesData);
+		
+		List<SeasonsData> seasons = new ArrayList<>();
+		
+		for (int i = 1; i <= seriesData.seasons(); i++) {
+			json = apiConsumer.getApiData(BASEURL + serieName.replace(" ", "+") + "&season=" + i +  APIKEY);
+			SeasonsData seasonsData = converter.getData(json, SeasonsData.class);
+			seasons.add(seasonsData);
+		}
+		seasons.forEach(System.out::println);
+	}
+}
